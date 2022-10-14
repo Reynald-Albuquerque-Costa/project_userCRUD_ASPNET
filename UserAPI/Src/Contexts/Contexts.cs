6ca334baf69d5038f74dd1e39430732063c7c79d
Reynald Albuquerque <reynaldalbuquerquecosta@gmail.com>
2022-10-14 01:30:11 -0300

    @Reynald-Albuquerque-Costa[Creating model and context]

---
+++ UserAPI/Src/Contexts/Contexts.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserAPI.Src.Model;
+
+namespace UserAPI.Src.Contexts
+{
+    public class UserContext : DbContext
+    {
+        #region Atributos
+        public DbSet<User> Users { get; set; }
+
+
+        #endregion
+
+
+        #region Constructors
+        public UserContext(DbContextOptions<UserContext> opt) :base(opt)
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ UserAPI/Src/Models/UserModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserAPI.Src.Model
+{
+    [Table("tb_users")]
+    public class User
+    {
+        #region Atributos
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        #endregion
+
+
+    }
+}
