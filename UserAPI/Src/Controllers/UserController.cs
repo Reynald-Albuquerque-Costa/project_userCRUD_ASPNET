038ec91217c1284a1bdceef93303825997815c7e
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-10-14 23:43:15 -0300

    @Reynald-Albuquerque-Costa[Controller and documenting]

---
+++ UserAPI/Src/Controllers/UserController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+using UserAPI.Src.Model;
+using UserAPI.Src.Repositories;
+
+
+namespace UserAPI.Src.Controllers
+{
+    [ApiController]
+    [Route("api/Users")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        #region Attributes
+
+        private readonly IUser _repository;
+
+
+        #endregion
+
+
+        #region Constructors
+        public UserController(IUser repository)
+        {
+            _repository = repository;
+
+
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Criar novo usuário
+        /// </summary>
+        [HttpPost]
+        public async Task<ActionResult> NovoUsuarioAsync([FromBody] User user)
+        {
+            await _repository.NovoUsuarioAsync(user);
+
+            return Created($"api/Usuarios", user);
+        }
+
+        /// <summary>
+        /// Pegar usuario pelo Email
+        /// </summary>
+        [HttpGet("email/{emailUser}")]
+        public async Task<ActionResult> PegarUsuarioPeloEmailAsync([FromRoute] string email)
+        {
+            try
+            {
+                return Ok(await _repository.PegarUsuarioPeloEmailAsync(email));
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { Mensagem = ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// Atulizar usuario
+        /// </summary>
+        [HttpPut]
+        public async Task<ActionResult> AtualizarUsuarioAsync([FromBody] User user)
+        {
+            try
+            {
+                await _repository.AtualizarUsuarioAsync(user);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { Mensagem = ex.Message });
+            }
+        }
+
+        /// <summary>
+        /// Deletar usuario pelo ID
+        /// </summary>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeletarUsuario([FromRoute] int id)
+        {
+            try
+            {
+                await _repository.DeletarUsuarioAsync(id);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(new { Mensagem = ex.Message });
+            }
+        }
+
+        #endregion
+    }
+}
--- UserAPI/Startup.cs
+++ UserAPI/Startup.cs
@@ -6,9 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using UserAPI.Src.Contexts;
 using UserAPI.Src.Repositories;
@@ -35,8 +38,54 @@ namespace UserAPI
             services.AddScoped<IUser, UserRepository>();
 
             // Controllers
+            services.AddCors();
             services.AddControllers();
+
+            // Swagger configuration
+            services.AddSwaggerGen(
+            s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo { Title = "UserCRUD", Version = "v1" });
+
+                s.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme()
+                    {
+                        Name = "Authorization",
+                        Type = SecuritySchemeType.ApiKey,
+                        Scheme = "Bearer",
+                        BearerFormat = "JWT",
+                        In = ParameterLocation.Header,
+                        Description = "JWT authorization header utiliza: Bearer + JWT Token",
+                    }
+                    );
+
+                s.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                             {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    }
+                    );
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                s.IncludeXmlComments(xmlPath);
+            }
+            );
+
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserContext context)
@@ -46,19 +95,31 @@ namespace UserAPI
             {
                 context.Database.EnsureCreated();
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserCRUD v1");
+                    c.RoutePrefix = string.Empty;
+                });
             }
 
-            // Production environment
-            context.Database.EnsureCreated();
+                // Production environment
+                // Routes
+                context.Database.EnsureCreated();
 
-            app.UseRouting();
+                app.UseRouting();
 
-            app.UseAuthorization();
+                app.UseCors(c => c
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+                app.UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+         }
     }
 }
